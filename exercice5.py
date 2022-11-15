d8ed3858fa09a8e4c16f5f83bf265359dad046b5
PearlClone <benjaminlecomte37@gmail.com>
2022-11-15 11:58:48 +0100

    update

--- exercice5.py
+++ exercice5.py
@@ -1,9 +1,13 @@
-def taille(x, t = 0) :
+def taille(x) :
     """
     Renvoie le nombre de chiffres de x en base 2
     """
-    if x <= 1 :
-        return t+1
-    else :
-        t += 1
-        return taille(x//2, t)
+    n = 1
+    while x > 0:
+        x //= 2
+        n = 1
+    
+    return n
+
+
+
